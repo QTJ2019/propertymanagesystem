544003a6e602de5a5845419b792c4f181feccd91
QTJ <853890233@qq.com>
2020-12-12 22:00:22 +0800

    后端搭建了springboot的环境，但还没安装Mybatis-puplus“

---
+++ backend/src/main/java/com/jian/propertymanagesystem/PropertymanagesystemApplication.java
@@ -0,0 +1,13 @@
+package com.jian.propertymanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertymanagesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertymanagesystemApplication.class, args);
+    }
+
+}
